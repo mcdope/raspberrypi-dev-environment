28d00f0fa5060703ea931acee73a613aa84b6aac
TobiasBaeumer@gmail.com <TobiasBaeumer@gmail.com>
2019-02-10 13:31:49 +0100

    update: grow image to have more room

--- update.sh
+++ update.sh
@@ -9,3 +9,6 @@ wget https://downloads.raspberrypi.org/raspbian_lite_latest
 unzip -o raspbian_lite_latest
 mv *-raspbian-stretch-lite.img raspbian-stretch-lite.img #to make it have a known name so we dont need to grep around
 rm raspbian_lite_latest
+
+# Grow image so we have more room for custom stuff
+sudo qemu-img resize raspbian-stretch-lite.img +2G #adjust size if needed
