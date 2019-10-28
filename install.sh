0ae74efd5014e5f9ba7ee537e0f79dce91c7b1dc
TobiasBaeumer@gmail.com <TobiasBaeumer@gmail.com>
2019-10-28 19:53:13 +0100

    install.sh: add message about how to become root from user context

--- install.sh
+++ install.sh
@@ -14,4 +14,6 @@ sudo apt-get install -y qemu qemu-system git wget unzip kpartx fdisk libarchive-
 echo
 echo "Done!"
 echo "You can now boot the emulated enviroment by running boot.sh and logging in with user alarm and password alarm (root password is root)."
+echo "To become root, you can either use 'sudo' or 'su', whatever you prefer."
+echo
 echo "... but your ssh key was installed too, so you shouldn't need these."
