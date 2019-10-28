d03df15c52825543df7a4777b7a5ffcc916a08f8
TobiasBaeumer@gmail.com <TobiasBaeumer@gmail.com>
2019-10-28 19:52:42 +0100

    boot.sh: optimize qemu configuration

--- boot.sh
+++ boot.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
-CPUCORES_TOTAL=`getconf _NPROCESSORS_ONLN`
-CPUCORES_QEMU=$((CPUCORES_TOTAL/2)) # we only use half of all cores to prevent stalling the host
+CPUCORES=`getconf _NPROCESSORS_ONLN`
 KEYBOARD_LAYOUT=`setxkbmap -query | grep layout | cut -d' ' -f6`
 
-echo "Will start VM now, but a quick warning ahead: CTRL-C will NOT be send to VM but instead will close the VM without shutdown!"
+echo "WARNING: You can login in the serial terminal that opens, but don't dare to press CTRL+C!"
+echo " (it would terminate the VM without proper shutdown, expect your VM to break...)"
+echo
+echo "For that reason I highly recommend to login in the graphical window (titled 'QEMU - raspberrypi-dev-environment')!"
+echo
+
 sleep 5
 
 x-terminal-emulator -e "qemu-system-aarch64 \
   -kernel arch_bootpart/Image.gz \
   -initrd arch_bootpart/initramfs-linux.img \
+  -append \"rw root=/dev/vda2 console=ttyAMA0 loglevel=8 rootwait fsck.repair=yes memtest=1 audit=0 CONFIG_DRM=y CONFIG_DRM_VIRTIO_GPU=y\" \
   -m 1024 -M virt \
   -cpu cortex-a53 \
+  -smp cpus=$CPUCORES \
+  -k $KEYBOARD_LAYOUT \
+  -name raspberrypi-dev-environment \
   -serial stdio \
-  -append \"rw root=/dev/vda2 console=ttyAMA0 loglevel=8 rootwait fsck.repair=yes memtest=1 audit=0\" \
-  -hda arch-pimirror.img \
+  -drive file=arch-pimirror.img,if=virtio,cache=none \
   -netdev user,id=net0,hostfwd=tcp::5022-:22 \
   -device virtio-net-device,netdev=net0 \
-  -no-reboot
-  -accel kvm,thread=multi
-  -smp cpus=$CPUCORES_QEMU
-  -k $KEYBOARD_LAYOUT
-  -name raspberrypi-dev-environment
-  -no-quit"
+  -device virtio-gpu-pci \
+  -no-reboot \
+  -no-quit; sleep 30"
